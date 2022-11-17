c57d825d41933346ce3f4ee0d8962bf13acc869e
Nikolajr93 <nikolajr93@gmail.com>
2022-11-17 13:54:23 +0100

    Commiting package one.

---
+++ src/main/java/rs/raf/packageOne/MyClassTwo.java
@@ -0,0 +1,14 @@
+package rs.raf.packageOne;
+
+public class MyClassTwo {
+
+    private Object MyAttributeTwo;
+
+    public Object getMyAttributeTwo() {
+        return MyAttributeTwo;
+    }
+
+    public void setMyAttributeTwo(Object myAttributeTwo) {
+        MyAttributeTwo = myAttributeTwo;
+    }
+}

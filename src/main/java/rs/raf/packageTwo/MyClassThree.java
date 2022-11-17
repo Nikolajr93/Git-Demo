39a3ea29603197adb6e1fd69375f2a16c5d79741
Nikolajr93 <nikolajr93@gmail.com>
2022-11-17 13:57:53 +0100

    Adding MyClasThree.

---
+++ src/main/java/rs/raf/packageTwo/MyClassThree.java
@@ -0,0 +1,14 @@
+package rs.raf.packageTwo;
+
+public class MyClassThree {
+
+    private Object MyAttributeThree;
+
+    public Object getMyAttributeThree() {
+        return MyAttributeThree;
+    }
+
+    public void setMyAttributeThree(Object myAttributeThree) {
+        MyAttributeThree = myAttributeThree;
+    }
+}

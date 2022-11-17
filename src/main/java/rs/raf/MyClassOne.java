e0a83677722311113c1a3691e3f3459bff835b04
Nikolajr93 <nikolajr93@gmail.com>
2022-11-17 13:46:38 +0100

    Adding MyClassOne.

---
+++ src/main/java/rs/raf/MyClassOne.java
@@ -0,0 +1,14 @@
+package rs.raf;
+
+public class MyClassOne {
+
+    private Object MyAttribureOne;
+
+    public Object getMyAttribureOne() {
+        return MyAttribureOne;
+    }
+
+    public void setMyAttribureOne(Object myAttribureOne) {
+        MyAttribureOne = myAttribureOne;
+    }
+}
